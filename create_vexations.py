42306eb2617c7407cd03dacd11cc0f3b8877a961
kazukib <b.kazuki@gmail.com>
2019-07-20 13:40:06 +0900

    first commit

---
+++ create_vexations.py
@@ -0,0 +1,28 @@
+#Vexations
+
+#NOTE DE L'AUTEUR:
+#Pour se jouer 840 fois de suite ce motif, il sera bon de se préparer au
+#préalable, et dans le plus grand silence, par des immobilités serieuses.
+
+f = open('vexations.py', 'w')
+for i in range(840 * 40):
+    f.write('print("ドラミフラット")\n')
+    f.write('print("ドシャープファナチュラル　ファ")\n')
+    f.write('print("ラシャープミナチュラル　ラシャープ")\n')
+    f.write('print("レフラットファ　シフラット")\n')
+    f.write('print("レシャープシシャープファシャープ")\n')
+    f.write('print("シフラットミ　ソ")\n')
+    f.write('print("レナチュラルシナチュラルファナチュラル")\n')
+    f.write('print("ドナチュラルミフラットラ")\n')
+    f.write('print("レシャープシシャープファシャープ")\n')
+    f.write('print("シフラットフラットミフラット　ソフラット")\n')
+    f.write('print("ラシャープミナチュラル　ドシャープ")\n')
+    f.write('print("ラフラットレシャープ　ファナチュラル")\n')
+    f.write('print("ソシャープレナチュラル　シナチュラル")\n')
+    f.write('print("ドフラットシフラットフラットラフラット")\n')
+    f.write('print("ミナチュラルドシャープソナチュラル")\n')
+    f.write('print("ミドソ")\n')
+    f.write('print("ミドソ")\n')
+    f.write('print("休符")\n')
+
+f.close()
